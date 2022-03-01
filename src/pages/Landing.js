7c018891dd9b144d29348b0a914e57f5655d4440
danielcm585 <danielcm585@gmail.com>
2022-03-01 15:55:01 +0700

    added landing page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -5,7 +5,7 @@ import "../styles/Landing.css"
 import LandingImg from "../images/Landing.png"
 import { Navbar, Footer } from "../components"
 
-import { Button, Flex, SimpleGrid, Text } from "@chakra-ui/react"
+import { Box, Button, Flex, SimpleGrid, Text } from "@chakra-ui/react"
 
 export default function Landing() {
   useState(() => {
@@ -42,6 +42,55 @@ export default function Landing() {
         </div>
         <div className="orange"></div>
         <img className="image" src={LandingImg} />
+      </Flex>
+      <Flex mt="90" justifyContent="center">
+        <Text fontSize="3xl" fontWeight="bold">Ribuan Bidang Pekerjaan</Text>
+      </Flex>
+      <Flex mt="10" justifyContent="center">
+        <SimpleGrid w="60%" columns="4" spacing="10">
+          <Box p="1" border="2px" borderColor="#FF8450" borderRadius="50">
+            <Flex justifyContent="center">
+              <Text fontSize="xl" fontWeight="bold">Guru</Text>
+            </Flex>
+          </Box>
+          <Box p="1" border="2px" borderColor="#FF8450" borderRadius="50">
+            <Flex justifyContent="center">
+              <Text fontSize="xl" fontWeight="bold">Desainer</Text>
+            </Flex>
+          </Box>
+          <Box p="1" border="2px" borderColor="#FF8450" borderRadius="50">
+            <Flex justifyContent="center">
+              <Text fontSize="xl" fontWeight="bold">Fotografer</Text>
+            </Flex>
+          </Box>
+          <Box p="1" border="2px" borderColor="#FF8450" borderRadius="50">
+            <Flex justifyContent="center">
+              <Text fontSize="xl" fontWeight="bold">Web Developer</Text>
+            </Flex>
+          </Box>
+        </SimpleGrid>
+      </Flex>
+      <Flex mt="8" justifyContent="center">
+        <SimpleGrid w="45%" columns="3" spacing="10">
+          <Box p="1" border="2px" borderColor="#FF8450" borderRadius="50">
+            <Flex justifyContent="center">
+              <Text fontSize="xl" fontWeight="bold">Translator</Text>
+            </Flex>
+          </Box>
+          <Box p="1" border="2px" borderColor="#FF8450" borderRadius="50">
+            <Flex justifyContent="center">
+              <Text fontSize="xl" fontWeight="bold">Helper</Text>
+            </Flex>
+          </Box>
+          <Box p="1" border="2px" borderColor="#FF8450" borderRadius="50">
+            <Flex justifyContent="center">
+              <Text fontSize="xl" fontWeight="bold">Driver</Text>
+            </Flex>
+          </Box>
+        </SimpleGrid>
+      </Flex>
+      <Flex mt="90" justifyContent="center">
+        <Text fontSize="3xl" fontWeight="bold">Cara Kerja di Rekrut.id</Text>
       </Flex>
       <Footer />  
     </>
