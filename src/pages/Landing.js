789ffbf26a39a94bcd47422fa3e60def9412465c
danielcm585 <danielcm585@gmail.com>
2022-03-01 17:31:03 +0700

    added landing page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -2,10 +2,10 @@ import React, { useState } from "react"
 
 import "../styles/Landing.css"
 
-import LandingImg from "../images/Landing.png"
+import { LandingImg, Procedure1Img } from "../images"
 import { Navbar, Footer } from "../components"
 
-import { Box, Button, Flex, SimpleGrid, Text } from "@chakra-ui/react"
+import { Box, Button, Flex, Image, SimpleGrid, Text } from "@chakra-ui/react"
 
 export default function Landing() {
   useState(() => {
@@ -50,22 +50,22 @@ export default function Landing() {
         <SimpleGrid w="60%" columns="4" spacing="10">
           <Box p="1" border="2px" borderColor="#FF8450" borderRadius="50">
             <Flex justifyContent="center">
-              <Text fontSize="xl" fontWeight="bold">Guru</Text>
+              <Text fontSize="xl" fontWeight="semibold">Guru</Text>
             </Flex>
           </Box>
           <Box p="1" border="2px" borderColor="#FF8450" borderRadius="50">
             <Flex justifyContent="center">
-              <Text fontSize="xl" fontWeight="bold">Desainer</Text>
+              <Text fontSize="xl" fontWeight="semibold">Desainer</Text>
             </Flex>
           </Box>
           <Box p="1" border="2px" borderColor="#FF8450" borderRadius="50">
             <Flex justifyContent="center">
-              <Text fontSize="xl" fontWeight="bold">Fotografer</Text>
+              <Text fontSize="xl" fontWeight="semibold">Fotografer</Text>
             </Flex>
           </Box>
           <Box p="1" border="2px" borderColor="#FF8450" borderRadius="50">
             <Flex justifyContent="center">
-              <Text fontSize="xl" fontWeight="bold">Web Developer</Text>
+              <Text fontSize="xl" fontWeight="semibold">Web Developer</Text>
             </Flex>
           </Box>
         </SimpleGrid>
@@ -74,17 +74,17 @@ export default function Landing() {
         <SimpleGrid w="45%" columns="3" spacing="10">
           <Box p="1" border="2px" borderColor="#FF8450" borderRadius="50">
             <Flex justifyContent="center">
-              <Text fontSize="xl" fontWeight="bold">Translator</Text>
+              <Text fontSize="xl" fontWeight="semibold">Translator</Text>
             </Flex>
           </Box>
           <Box p="1" border="2px" borderColor="#FF8450" borderRadius="50">
             <Flex justifyContent="center">
-              <Text fontSize="xl" fontWeight="bold">Helper</Text>
+              <Text fontSize="xl" fontWeight="semibold">Helper</Text>
             </Flex>
           </Box>
           <Box p="1" border="2px" borderColor="#FF8450" borderRadius="50">
             <Flex justifyContent="center">
-              <Text fontSize="xl" fontWeight="bold">Driver</Text>
+              <Text fontSize="xl" fontWeight="semibold">Driver</Text>
             </Flex>
           </Box>
         </SimpleGrid>
@@ -92,7 +92,44 @@ export default function Landing() {
       <Flex mt="90" justifyContent="center">
         <Text fontSize="3xl" fontWeight="bold">Cara Kerja di Rekrut.id</Text>
       </Flex>
-      <Footer />  
+      <Flex mt="8" justifyContent="center">
+        <SimpleGrid w="85%" columns="3" spacing="10">
+          <Box p="2" pb="3" borderRadius="md" shadow="md">
+            <Image src={Procedure1Img} borderRadius="md" />
+            <Flex mt="2" justifyContent="center">
+              <Text fontSize="xl" fontWeight="bold">1. Jelaskan Kebutuhanmu</Text>
+            </Flex>
+            <Flex mt="2" justifyContent="center">
+              <Text align="center" fontSize="lg">
+                Informasikan jasa yang Anda butuhkan. Dapatkan lebih dari 10+ penawaran dalam hitungan jam
+              </Text>
+            </Flex>
+          </Box>
+          <Box p="2" pb="3" borderRadius="md" shadow="md">
+            <Image src={Procedure1Img} borderRadius="md" />
+            <Flex mt="2" justifyContent="center">
+              <Text fontSize="xl" fontWeight="bold">2. Bandingkan Setiap Jasa</Text>
+            </Flex>
+            <Flex mt="2" justifyContent="center">
+              <Text align="center" fontSize="lg">
+                Bandingkan profil untuk menemukan pilihan yang sesuai
+              </Text>
+            </Flex>
+          </Box>
+          <Box p="2" pb="3" borderRadius="md" shadow="md">
+            <Image src={Procedure1Img} borderRadius="md" />
+            <Flex mt="2" justifyContent="center">
+              <Text fontSize="xl" fontWeight="bold">3. Rekrut Pekerja</Text>
+            </Flex>
+            <Flex mt="2" justifyContent="center">
+              <Text align="center" fontSize="lg">
+                Rekrut pekerja yang paling tepat untuk Anda. Bekerja melalui workspace kami hingga tuntas.
+              </Text>
+            </Flex>
+          </Box>
+        </SimpleGrid>
+      </Flex>
+      <Footer />
     </>
   )
 }
