fac5979a6f46b54637bdcc936c6fc32bb87ece61
danielcm585 <danielcm585@gmail.com>
2022-03-26 00:40:25 +0700

    created resopnsive design for profile page

--- src/components/profile/ProfileDetails.js
+++ src/components/profile/ProfileDetails.js
@@ -1,5 +1,6 @@
 import React from "react"
 
+import { FaEdit } from "react-icons/fa"
 import { IoMdDocument } from "react-icons/io"
 import { MdPhone, MdMail } from "react-icons/md"
 
@@ -7,93 +8,118 @@ import { ProfileForm } from "./";
 import { ChooseJob } from "../job";
 import { Star } from "../review"
 
-import { useDisclosure } from "@chakra-ui/react";
+import { IconButton, useDisclosure, useMediaQuery } from "@chakra-ui/react";
 import { Avatar, Box, Text, HStack, Spacer, Link, Icon, Button, Flex } from "@chakra-ui/react"
 
 export default function ProfileDetails({ user, me, client }) {
   const { isOpen, onOpen, onClose } = useDisclosure()
 
+  const [ isBigScreen ] = useMediaQuery("(min-width:600px)")
+
   return (
     <>
-      <Flex w="100%">
-        <Flex direction="column">
-          <Flex>
-            <Avatar mr="5" h="165px" w="165px" src={user.profPic} borderRadius="lg" />
-            <Flex direction="column">
-              <Link href={"/profile/"+user.id} isExternal>
-                <Text fontSize="2xl" fontWeight="semibold">{user.name}</Text>
-              </Link>
+      <Box w="100%">
+        <Flex w="100%">
+          <Flex direction="column">
+            <Flex>
               {
-                (user.category != "-") &&
-                  <Text fontSize="lg">{user.category}</Text> 
+                isBigScreen ? 
+                  <Avatar mr="5" h="165px" w="165px" src={user.profPic} borderRadius="lg" /> : 
+                  <Avatar mr="5" h="135px" w="135px" src={user.profPic} borderRadius="lg" />
               }
-              {
-                (user.rating != null && user.rating > 0) && 
-                  <Star rating={user.rating} />
-              }
-              <Link href={"mailto:"+user.email} isExternal>
-                <HStack>
-                  <Icon as={MdMail} color="#FF8450" />
-                  <Text>{user.email}</Text>
-                </HStack>
-              </Link>
-              <Link href={"https://wa.me/"+user.phone} isExternal>
-                <HStack>
-                  <Icon as={MdPhone} color="#FF8450" />
-                  <Text>{user.phone}</Text>
-                </HStack>
-              </Link>
-              {
-                (user.role == "worker") && (
-                  <Link href={"/cv/"+user.id} isExternal>
-                    <HStack>
-                      <Icon as={IoMdDocument} color="#FF8450" />
-                      <Text>{"CV "+user.name}</Text>
-                    </HStack>
-                  </Link>
-                )
-              }
-            </Flex>
-            <Spacer></Spacer>
-          </Flex>
-          {
-            (user.bio != null) && (
-              <Box mt="4">
+              <Flex direction="column">
+                <Link href={"/profile/"+user.id} isExternal>
+                  <Text fontSize="2xl" fontWeight="semibold">{user.name}</Text>
+                </Link>
                 {
-                  user.bio.split('\n').map((par, idx) => 
-                    <Text key={idx} mt="1">{par}</Text>
+                  (user.category != "-") &&
+                    <Text fontSize="lg">{user.category}</Text> 
+                }
+                {
+                  (user.rating != null && user.rating > 0) && 
+                    <Star rating={user.rating} />
+                }
+                <Link href={"mailto:"+user.email} isExternal>
+                  <HStack>
+                    <Icon as={MdMail} color="#FF8450" />
+                    <Text>{user.email}</Text>
+                  </HStack>
+                </Link>
+                <Link href={"https://wa.me/"+user.phone} isExternal>
+                  <HStack>
+                    <Icon as={MdPhone} color="#FF8450" />
+                    <Text>{user.phone}</Text>
+                  </HStack>
+                </Link>
+                {
+                  (user.role == "worker") && (
+                    <Link href={"/cv/"+user.id} isExternal>
+                      <HStack>
+                        <Icon as={IoMdDocument} color="#FF8450" />
+                        <Text>{"CV "+user.name}</Text>
+                      </HStack>
+                    </Link>
                   )
                 }
-              </Box>
+              </Flex>
+              <Spacer></Spacer>
+            </Flex>
+            {
+              (user.bio != null) && (
+                <Box mt="4">
+                  {
+                    user.bio.split('\n').map((par, idx) => 
+                      <Text key={idx} mt="1">{par}</Text>
+                    )
+                  }
+                </Box>
+              )
+              // <Text mt="5">{user.bio}</Text>
+            }
+          </Flex>
+          {
+            me && (
+              <>
+                <ProfileForm isOpen={isOpen} onClose={onClose} user={user} />
+                <Spacer></Spacer>
+                {
+                  isBigScreen ? (
+                    <Button pl="8" pr="8" variant="ghost" borderRadius="50"
+                      onClick={() => onOpen()}>
+                      Edit Profil
+                    </Button>
+                  ) : (
+                    <IconButton variant="ghost" isRounded icon={<FaEdit />} />
+                  )
+                }
+              </>
             )
-            // <Text mt="5">{user.bio}</Text>
+          }
+          {
+            (client && user.role == "worker" && isBigScreen) && (
+              <>
+                <ChooseJob isOpen={isOpen} onClose={onClose} worker={user} />
+                <Spacer></Spacer>
+                <Button ml="2" pl="10" pr="10" borderRadius="50" bgColor="#FF8450"
+                  onClick={() => onOpen()}>
+                  <Text fontSize="sm" fontWeight="bold">Undang</Text>
+                </Button> 
+              </>
+            )
           }
         </Flex>
         {
-          me && (
-            <>
-              <ProfileForm isOpen={isOpen} onClose={onClose} user={user} />
-              <Spacer></Spacer>
-              <Button pl="8" pr="8" variant="ghost" borderRadius="50"
-                onClick={() => onOpen()}>
-                Edit Profil
-              </Button>
-            </>
-          )
-        }
-        {
-          (client && user.role == "worker") && (
+          (client && user.role == "worker" && !isBigScreen) && (
             <>
               <ChooseJob isOpen={isOpen} onClose={onClose} worker={user} />
-              <Spacer></Spacer>
-              <Button ml="2" pl="10" pr="10" borderRadius="50" bgColor="#FF8450"
+              <Button w="100%" mt="5" pl="10" pr="10" borderRadius="50" bgColor="#FF8450"
                 onClick={() => onOpen()}>
                 <Text fontSize="sm" fontWeight="bold">Undang</Text>
               </Button> 
             </>
           )
         }
-      </Flex>
+      </Box>
     </>
   )
 }
