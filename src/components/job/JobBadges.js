ca9c46d2b15293ec422f2f80eb2c6dfef01b3eaf
danielcm585 <danielcm585@gmail.com>
2022-03-26 00:04:01 +0700

    created responsive design for job card

--- src/components/job/JobBadges.js
+++ src/components/job/JobBadges.js
@@ -2,35 +2,37 @@ import React from "react"
 
 import { BiStar, BiTimeFive, BiLike } from "react-icons/bi"
 
-import { Icon, Badge } from "@chakra-ui/react"
+import { Icon, Badge, HStack } from "@chakra-ui/react"
 
 export default function JobBadges({ job }) {
   return (
     <>
-      {
-        (job.salary > 5000000) && (
-          <Badge size="sm" colorScheme="orange">
-            <Icon mr="1" as={BiLike} />
-            Best Offer
-          </Badge>
-        )
-      }
-      {
-        (job.author.rating != null && job.author.rating > 4) && (
-          <Badge size="sm" colorScheme="green">
-            <Icon mr="1" as={BiStar} />
-            Great Company
-          </Badge>
-        )
-      }
-      {
-        (job.registrants.length > 5) && (
-          <Badge size="sm" colorScheme="red">
-            <Icon mr="1" as={BiTimeFive} />
-            Limited Time
-          </Badge>
-        )
-      }
+      <HStack>
+        {
+          (job.salary > 5000000) && (
+            <Badge size="sm" colorScheme="orange">
+              <Icon mr="1" as={BiLike} />
+              Best Offer
+            </Badge>
+          )
+        }
+        {
+          (job.author.rating != null && job.author.rating > 4) && (
+            <Badge size="sm" colorScheme="green">
+              <Icon mr="1" as={BiStar} />
+              Great Company
+            </Badge>
+          )
+        }
+        {
+          (job.registrants.length > 5) && (
+            <Badge size="sm" colorScheme="red">
+              <Icon mr="1" as={BiTimeFive} />
+              Limited Time
+            </Badge>
+          )
+        }
+      </HStack>
     </>
   )
 }
--- src/components/job/JobCard.js
+++ src/components/job/JobCard.js
@@ -6,8 +6,8 @@ import { FaMoneyBillWave } from "react-icons/fa"
 import { JobBadges } from "."
 import { Star } from "../review"
 
-import { useToast } from "@chakra-ui/react"
-import { Box, Button, HStack, Link, Spacer, Text, Flex, Icon, Avatar } from "@chakra-ui/react"
+import { useToast, useMediaQuery } from "@chakra-ui/react"
+import { Box, Button, HStack, Link, Spacer, Text, Flex, Icon, Avatar, SimpleGrid } from "@chakra-ui/react"
 
 export default function JobCard({ job, preview, worker }) {
   const parseAmount = (amount) => {
@@ -22,6 +22,115 @@ export default function JobCard({ job, preview, worker }) {
     isClosable: true
   })
 
+  const [ isBigScreen ] = useMediaQuery("(min-width:600px)")
+
+  if (!isBigScreen) return (
+    <>
+      <Box p="2" mb="2" shadow="md" borderRadius="md"
+        _hover={{ bgColor: "gray.50" }}
+        onClick={() => window.location.href="/job/"+job._id}>
+        <Flex>
+          <Flex p="2">
+            <Avatar h="95px" w="95px" borderRadius="md" src={job.author.profPic} />
+          </Flex>
+          <Box ml="2" mt="2">
+            {
+              (!preview) && 
+                <JobBadges job={job} />
+            }
+            <HStack spacing="2">
+              <Text fontSize="lg" fontWeight="semibold">{job.title}</Text>
+            </HStack>
+            <Flex>
+              <Link href={"/profile/"+job.author._id}>
+                <Text fontSize="md" color="gray.600">{job.author.name}</Text>
+              </Link>
+            </Flex>
+            {
+              (job.author.rating != null && job.author.rating > 0) &&  
+                <Star rating={job.author.rating} />
+            }
+            <Text fontSize="sm" color="gray.600">
+              {
+                (job.status == "HIRING") ? job.registrants.length+" Pelamar" : (
+                  (job.status == "WAITING") ? "Menunggu Konfirmasi Pekerja" : (
+                    (job.status == "ONGOING") ? "Dalam Pengerjaan" : (
+                      (job.status == "REVIEWING") ? "Dalam Proses Review" : (
+                        (job.status == "DONE") ? "Pekerjaan Selesai" : ""
+                      )
+                    )
+                  )
+                )
+              }
+            </Text>
+          </Box>
+        </Flex>
+        {
+          (!preview) ? (
+            <>
+              <HStack mt="1">
+                <Box>
+                  <HStack>
+                    <Icon as={MdLocationOn} color="gray.600" />
+                    <Text color="gray.600">Lokasi</Text>
+                  </HStack>
+                  <HStack mt="1">
+                    <Icon as={MdWork} color="gray.600" />
+                    <Text color="gray.600">Job Type</Text>
+                  </HStack>
+                  <HStack mt="1">
+                    <Icon as={FaMoneyBillWave} color="gray.600" />
+                    <Text color="gray.600">Salary</Text>
+                  </HStack>
+                </Box>
+                <Box pl="7">
+                  <Text fontWeight="semibold">{job.location}</Text>
+                  <Text mt="1" fontWeight="semibold">{job.jobType}</Text>
+                  <Text mt="1" fontWeight="semibold">{parseAmount(job.salary)}</Text>
+                </Box>
+              </HStack>
+            </>
+          ) : (
+            <>
+              {/* FIXME: Maybe need some fix */}
+              <Spacer></Spacer>
+              <Button pl="10" pr="10" borderRadius="50" bgColor="#FF8450" isLoading={isLoading}
+                onClick={() => {
+                  setIsLoading(true)
+                  fetch("https://protected-castle-75235.herokuapp.com/client/offer", {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    credentials: "include",
+                    body: JSON.stringify({
+                      worker: worker._id,
+                      job: job._id
+                    })
+                  })
+                  .then(resp => resp.json())
+                  .then(json => {
+                    if (json.statusCode >= 400) throw new Error(json.message)
+                    setIsLoading(false)
+                    toast({
+                      title: "Undangan berhasil dikirim",
+                      status: "success"
+                    })
+                  })
+                  .catch((err) => {
+                    setIsLoading(false)
+                    toast({
+                      title: err.message,
+                      status: "error"
+                    })
+                  })
+                }}>
+                Undang
+              </Button>
+            </>
+          )
+        }
+      </Box>
+    </>
+  )
   return (
     <>
       <Box p="2" mb="2" shadow="md" borderRadius="md"
