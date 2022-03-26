aa1e720496408cc8c935b3b4782a28c7ab3ac60e
danielcm585 <danielcm585@gmail.com>
2022-03-26 09:34:03 +0700

    fixed job card design

--- src/components/job/JobBadges.js
+++ src/components/job/JobBadges.js
@@ -2,37 +2,46 @@ import React from "react"
 
 import { BiStar, BiTimeFive, BiLike } from "react-icons/bi"
 
-import { Icon, Badge, HStack } from "@chakra-ui/react"
+import { useMediaQuery } from "@chakra-ui/react"
+import { Icon, Badge, Flex } from "@chakra-ui/react"
 
 export default function JobBadges({ job }) {
+  const [ isBigScreen ] = useMediaQuery("(min-width:600px)")
+
   return (
     <>
-      <HStack>
+      <Flex direction={!isBigScreen && "column"}>
         {
           (job.salary > 5000000) && (
-            <Badge size="sm" colorScheme="orange">
-              <Icon mr="1" as={BiLike} />
-              Best Offer
-            </Badge>
+            <Flex>
+              <Badge size="sm" colorScheme="orange">
+                <Icon mr="1" as={BiLike} />
+                Best Offer
+              </Badge>
+            </Flex>
           )
         }
         {
           (job.author.rating != null && job.author.rating > 4) && (
-            <Badge size="sm" colorScheme="green">
-              <Icon mr="1" as={BiStar} />
-              Great Company
-            </Badge>
+            <Flex>
+              <Badge ml={isBigScreen && "1"} mt={!isBigScreen && "1"} size="sm" colorScheme="green">
+                <Icon mr="1" as={BiStar} />
+                Great Company
+              </Badge>
+            </Flex>
           )
         }
         {
           (job.registrants.length > 5) && (
-            <Badge size="sm" colorScheme="red">
-              <Icon mr="1" as={BiTimeFive} />
-              Limited Time
-            </Badge>
+            <Flex>
+              <Badge ml={isBigScreen && "1"} mt={!isBigScreen && "1"} size="sm" colorScheme="red">
+                <Icon mr="1" as={BiTimeFive} />
+                Limited Time
+              </Badge>
+            </Flex>
           )
         }
-      </HStack>
+      </Flex>
     </>
   )
 }
